0bef779ff6238145cc6a80cbf83e1a385f0fd136
Marco Fortina <marco_fortina@hotmail.it>
2025-12-07 16:14:14 +0100

    fix(sysctl): adapt sysctl registration for kernel >= 6.4

--- src/rc_init.c
+++ src/rc_init.c
@@ -2766,6 +2766,7 @@ static struct ctl_table rcraid_table[] = {
 	{ }
 };
 
+#if LINUX_VERSION_CODE < KERNEL_VERSION(6,4,0)
 static struct ctl_table rcraid_dir_table[] = {
 	{ .procname	= "rcraid",
 	  .mode		= 0555,
@@ -2786,6 +2787,7 @@ static struct ctl_table rcraid_root_table[] = {
 	  .child	= rcraid_scsi_dir_table },
 	{ }
 };
+#endif
 
 static struct ctl_table_header *rcraid_sysctl_hdr;
 
@@ -2807,7 +2809,9 @@ static int __init rcraid_init(void)
 	if (err != 0)
 		return err;
 
-#if LINUX_VERSION_CODE >= KERNEL_VERSION(2,6,21)
+#if LINUX_VERSION_CODE >= KERNEL_VERSION(6,4,0)
+    rcraid_sysctl_hdr = register_sysctl("dev/scsi/rcraid", rcraid_table);
+#elif LINUX_VERSION_CODE >= KERNEL_VERSION(2,6,21)
 	rcraid_sysctl_hdr = register_sysctl_table(rcraid_root_table);
 #else
 	/*
